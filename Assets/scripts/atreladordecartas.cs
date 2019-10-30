682b9314eefa48eb8fb3e7f57d036146670a2e45
ARTHUR WYPYCH DE ALMEIDA <19.00565-2@maua.br>
2019-10-30 12:49:28 -0300

    tamo indo

--- Assets/scripts/atreladordecartas.cs
+++ Assets/scripts/atreladordecartas.cs
@@ -10,10 +10,10 @@ public class atreladordecartas : MonoBehaviour
     public Text textocarta2;
     public Text textocarta3;
     public Text textocarta4;
-    public double poder1 = 0;
-	public double poder2 = 0;
-	public double poder3 = 0;
-	public double poder4 = 0;
+    public float poder1 = 0;
+	public float poder2 = 0;
+	public float poder3 = 0;
+	public float poder4 = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,76 +23,76 @@ public class atreladordecartas : MonoBehaviour
         {
             case 1:
             textocarta1.text = "seu gordo";
-            poder1 = 0.2;
+            poder1 = 0.2f;
             break;
             case 2:
             textocarta1.text = "seu otario,n sabe 2 + 2";
-            poder1 = 0.3;
+            poder1 = 0.3f;
             break;
             case 3:
             textocarta1.text = "ninguem te quer";
-            poder1 = 0.3;
+            poder1 = 0.3f;
             break;
             case 4:
-            textocarta1.text = "what can i say except the n word";
-            poder1 = 0.7;
+            textocarta1.text = "vc vive numa sociedade";
+            poder1 = 0.7f;
             break;
         }
         switch(Random.Range(1,5))
         {
             case 1:
             textocarta2.text = "seu gordo";
-            poder2 = 0.2;
+            poder2 = 0.2f;
             break;
             case 2:
             textocarta2.text = "seu otario,n sabe 2 + 2";
-            poder2 = 0.3;
+            poder2 = 0.3f;
             break;
             case 3:
             textocarta2.text = "ninguem te quer";
-            poder2 = 0.3;
+            poder2 = 0.3f;
             break;
             case 4:
-            textocarta2.text = "what can i say except the n word";
-            poder2 = 0.7;
+            textocarta2.text = "vc vive numa sociedade";
+            poder2 = 0.7f;
             break;
         }
         switch(Random.Range(1,5))
         {
             case 1:
             textocarta3.text = "seu gordo";
-            poder3 = 0.2;
+            poder3 = 0.2f;
             break;
             case 2:
             textocarta3.text = "seu otario,n sabe 2 + 2";
-            poder3 = 0.3;
+            poder3 = 0.3f;
             break;
             case 3:
             textocarta3.text = "ninguem te quer";
-            poder3 = 0.3;
+            poder3 = 0.3f;
             break;
             case 4:
-            textocarta3.text = "what can i say except the n word";
-            poder3 = 0.7;
+            textocarta3.text = "vc vive numa sociedade";
+            poder3 = 0.7f;
             break;
         }
         switch(Random.Range(1,5))
         {
             case 1:
             textocarta4.text = "seu gordo";
-            poder4 = 0.2;
+            poder4 = 0.2f;
             break;
             case 2:
             textocarta4.text = "seu otario,n sabe 2 + 2";
-            poder4 = 0.3;
+            poder4 = 0.3f;
             break;
             case 3:
             textocarta4.text = "ninguem te quer";
-            poder4 = 0.3;
+            poder4 = 0.3f;
             break;
             case 4:
-            textocarta4.text = "what can i say except the n word";
-            poder4 = 0.7;
+            textocarta4.text = "vc vive numa sociedade";
+            poder4 = 0.7f;
             break;
         }
     }
--- Assets/scripts/clicacarta1.cs
+++ Assets/scripts/clicacarta1.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public class clicacarta1 : MonoBehaviour
 {
 	
+	public GameObject barraoppo;
 	
-	
+    public float aa = 0.1f;
+
 	void OnMouseDown()
 	{
-		
+		barraoppo.transform.localScale -= new Vector3 (GameObject.Find("cartas").GetComponent<atreladordecartas>().poder1, 0f, 0f);
 	}
 	
     // Start is called before the first frame update
